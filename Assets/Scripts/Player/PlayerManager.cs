3a7b58a215fdd46eda64d7c689af88cb7d0e3438
yanos1 <yanos241999@gmail.com>
2025-04-14 20:07:57 +0300

    first level graybox

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -8,14 +8,16 @@ namespace Player
 {
     public class PlayerManager : MonoBehaviour
     {
+        private PlayerMovement _playerMovement;
         private void Start()
         {
             CoreManager.Instance.Player = this;
+            _playerMovement = GetComponent<PlayerMovement>();
         }
 
         private void OnCollisionEnter2D(Collision2D other)
         {
-            if (other.gameObject.GetComponent<IBreakable>() is { } breakable)
+            if (other.gameObject.GetComponent<IBreakable>() is { } breakable && _playerMovement.IsDashing)
             {
                 breakable.OnHit((other.transform.position - gameObject.transform.position).normalized);
             }

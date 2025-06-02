033732fad290f9f245f07f09c6b7ba280e10f0ba
yanos1 <yanos241999@gmail.com>
2025-06-02 18:13:18 +0300

    new sounds

---
+++ Assets/Scripts/Triggers/DangerLayerTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Managers;
+using UnityEngine;
+
+namespace Triggers
+{
+    public class DangerLayerTrigger : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                CoreManager.Instance.AudioManager.SetGlobalParameter("Danger", 1);
+            }
+        }
+        
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                CoreManager.Instance.AudioManager.SetGlobalParameter("Danger", 0);
+            }
+        }
+    }
+}

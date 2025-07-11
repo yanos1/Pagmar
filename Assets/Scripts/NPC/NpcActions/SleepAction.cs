db0d8936c0945341b3d8101d24058e2358f4e713
yanos1 <yanos241999@gmail.com>
2025-07-11 13:09:23 +0300

    falldown update

--- Assets/Scripts/NPC/NpcActions/SleepAction.cs
+++ Assets/Scripts/NPC/NpcActions/SleepAction.cs
@@ -1,4 +1,6 @@
-﻿using NPC.BigFriend;
+﻿using FMODUnity;
+using Managers;
+using NPC.BigFriend;
 using UnityEngine;
 
 namespace NPC.NpcActions
@@ -10,11 +12,13 @@ namespace NPC.NpcActions
     public class SleepAction : NpcAction
     {
         [SerializeField] private BigActions _actions;
+        [SerializeField] private EventReference snoring;
         private bool displayingSleep = false;
         public override void StartAction(Npc npc)
         {
             base.StartAction(npc);
             npc.SetState(NpcState.Sleeping);
+            CoreManager.Instance.AudioManager.PlayOneShot(snoring, npc.transform.position);
         }
 
         public override void UpdateAction(Npc npc)
